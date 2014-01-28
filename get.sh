61dd6f4675fd3db4aa7259f1228da521f803ac9e
Kalentine Vamenek <aisseday@gmail.com>
2014-01-28 18:54:22 +0400

    mb to ft conversion

--- get.sh
+++ get.sh
@@ -1,9 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 LEFT_LON=35
 RIGHT_LON=40
 
 UP_LAN=56.5
 DOWN_LAN=54
+
+#TODO: чтение параметров из файла
+#TODO: пересчёт mib в футы (метры)
+#TODO: запись в postgres
+#TODO: кольцевой буфер заданной длины
+#TODO: документация
+
+declare -A convert_mb_to_ft
+convert_mb_to_ft["1000 mb"]="364"
+convert_mb_to_ft["925 mb"]="2498"
+convert_mb_to_ft["850 mb"]="4781"
+convert_mb_to_ft["700 mb"]="9882"
+convert_mb_to_ft["500 mb"]="18289"
+convert_mb_to_ft["400 mb"]="23574"
+convert_mb_to_ft["300 mb"]="30065"
+convert_mb_to_ft["250 mb"]="33999"
+convert_mb_to_ft["200 mb"]="38662"
 
 
 date_auto=`date -u +%Y%m%d`
@@ -19,22 +36,33 @@ date_auto="$date_auto"00;
 
 rm -f data.csv
 
-perl get_gfs.pl data $date_auto 0 24 6 $params $levels . # запуск скрипта скачивания grib-файлов
+# perl et_gfs.pl data $date_auto 0 24 6 $params $levels . # запуск скрипта скачивания grib-файлов
 
 for time in 06
 do
     grib_filename=gfs.t"$cycle"z.mastergrb2f"$time"
-    ./wgrib2 $grib_filename -csv $converted # декодирование grib-файла в csv формат
+
+    # декодирование grib-файла в csv формат
+    # ./wgrib2 $grib_filename -csv $converted 
+
     # фильтрация по координатам
-    cat $converted | awk -F ',' -v LL="$LEFT_LON" -v RL="$RIGHT_LON" -v UL="$UP_LAN" -v DL="$DOWN_LAN" '$5 >= LL && $5 <= RL && $6 >= DL && $6 <=UL' > $out 
+    # cat $converted | awk -F ',' -v LL="$LEFT_LON" -v RL="$RIGHT_LON" -v UL="$UP_LAN" -v DL="$DOWN_LAN" '$5 >= LL && $5 <= RL && $6 >= DL && $6 <=UL' > $out 
     
+    #сформировать имя выходного файла == дате, на которую делается данный прогноз
     data_filename=`head -1 out.csv | awk -F "," '{ print $2 }' | sed 's/"//g'` 
 
-    # далее для группировки строк с различными параметрами но одной координатной ячейкой
-    cat out.csv | awk -F "," '{ print $4 "," $5 "," $6}' | sort | uniq > coords.txt #получаем список координатных ячеек
+    # далее группировка строк с различными параметрами но одной координатной ячейкой
+    # cat out.csv | awk -F "," '{ print $4 "," $5 "," $6}' | sort | uniq > coords.txt #получаем список координатных ячеек (широта, долгота, высота)
     while read coord; do #обходим все ячейки
-        #Дата, долгота, широта, уровень, TMP, UGRD, VGRD
-        cat out.csv | fgrep ",$coord," | sed 's/$/,/' | xargs | paste -d , | awk -F "," '{ print $2 "," $5 "," $6 "," $4 "," $7 "," $14 "," $21 }' >> $data_filename.csv
+        #Выводим данные в формате: Дата, долгота, широта, уровень, TMP, UGRD, VGRD
+        cat out.csv | fgrep ",$coord," | sed 's/$/,/' | xargs | paste | awk -F "," '{ print $2 "," $5 "," $6 "," $4 "," $7 "," $14 "," $21 }' >> "$data_filename".csv
+
+        #конвертация mb в ft
+        for K in "${!convert_mb_to_ft[@]}";
+        do
+            sed -i "s/$K/${convert_mb_to_ft[$K]}/" "$data_filename".csv
+        done;
+
     done < coords.txt
 
 
@@ -42,8 +70,8 @@ done
 
 
 
-rm $converted
-rm $out
-rm coords.txt
-rm gfs*
+# rm $converted
+# rm $out
+# rm coords.txt
+# rm gfs*
 

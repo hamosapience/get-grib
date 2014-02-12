a32cc3f62ed8ff32d3d5aefe3e3e5424cf878ac9
Kalentine Vamenek <aisseday@gmail.com>
2014-02-13 03:44:46 +0400

    working version

--- get.sh
+++ get.sh
@@ -36,23 +36,23 @@ date_auto="$date_auto"00;
 
 rm -f data.csv
 
-# perl et_gfs.pl data $date_auto 0 24 6 $params $levels . # запуск скрипта скачивания grib-файлов
+perl get_gfs.pl data $date_auto 0 48 6 $params $levels . # запуск скрипта скачивания grib-файлов
 
-for time in 06
+for time in 06 12 18 24 30 36 42 48
 do
     grib_filename=gfs.t"$cycle"z.mastergrb2f"$time"
 
     # декодирование grib-файла в csv формат
-    # ./wgrib2 $grib_filename -csv $converted 
+     ./wgrib2 $grib_filename -csv $converted 
 
     # фильтрация по координатам
-    # cat $converted | awk -F ',' -v LL="$LEFT_LON" -v RL="$RIGHT_LON" -v UL="$UP_LAN" -v DL="$DOWN_LAN" '$5 >= LL && $5 <= RL && $6 >= DL && $6 <=UL' > $out 
+     cat $converted | awk -F ',' -v LL="$LEFT_LON" -v RL="$RIGHT_LON" -v UL="$UP_LAN" -v DL="$DOWN_LAN" '$5 >= LL && $5 <= RL && $6 >= DL && $6 <=UL' > $out 
     
     #сформировать имя выходного файла == дате, на которую делается данный прогноз
     data_filename=`head -1 out.csv | awk -F "," '{ print $2 }' | sed 's/"//g'` 
 
     # далее группировка строк с различными параметрами но одной координатной ячейкой
-    # cat out.csv | awk -F "," '{ print $4 "," $5 "," $6}' | sort | uniq > coords.txt #получаем список координатных ячеек (широта, долгота, высота)
+    cat out.csv | awk -F "," '{ print $4 "," $5 "," $6}' | sort | uniq > coords.txt #получаем список координатных ячеек (широта, долгота, высота)
     while read coord; do #обходим все ячейки
         #Выводим данные в формате: Дата, долгота, широта, уровень, TMP, UGRD, VGRD
         cat out.csv | fgrep ",$coord," | sed 's/$/,/' | xargs | paste | awk -F "," '{ print $2 "," $5 "," $6 "," $4 "," $7 "," $14 "," $21 }' >> "$data_filename".csv
@@ -70,8 +70,8 @@ done
 
 
 
-# rm $converted
-# rm $out
-# rm coords.txt
-# rm gfs*
+rm $converted
+rm $out
+rm coords.txt
+rm gfs*
 
